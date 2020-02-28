1b59b5df5aa0d50d56239069c4d32cb5ce1e4895
Yasmin Medeiros <yasmin.medeirosapp@gmail.com>
2020-02-28 08:30:00 -0300

    Iniciando o Roteiro3

---
+++ Roteiro3/Questao2/Headers/ContaEspecial.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <iostream>
+#include <iConta.h>
+#include<string.h>
+
+
+class ContaEspecial: public iConta
+{
+private:
+    
+public:
+    virtual void definirLimite();
+};
+
---
+++ Roteiro3/Questao2/Headers/iConta.h
@@ -0,0 +1,9 @@
+#pragma once
+#include <iostream>
+class iConta
+{
+public:
+    virtual void sacar(double valor);
+    virtual void depositar (double valor);
+    
+};
